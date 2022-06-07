bd337baae6d6fa8047c75b165dd8a58e4511bb14
infosecmentor <muhambiphares@gmail.com>
2022-06-07 20:19:21 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = "Phares";
+
+const welcomeMessage = `WELCOME TO FLATBOOK, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
